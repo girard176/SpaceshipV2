0d7d734c51d58c1dad3391e5837b9c18c1d4dc03
Andre <girard176@live.com>
2021-10-01 14:51:10 -0700

    Updated selectShipData for proper ship

--- main.js
+++ main.js
@@ -23,28 +23,36 @@ function updateShipList(){
     getShipData().then(function(data){
             console.log("Updating ship list: ")
             for(element in data.results){
-                let option = createOption(data.results[element].name);
+                let option = createOption(data.results[element].id, data.results[element].name);
                 shipList.appendChild(option);
             }
         }
     );
 }
 
-function createOption(text){
+function createOption(id, text){
     let option = document.createElement("option");
+    option.id = id;
     option.textContent = text;
     console.log(option.text);
+    console.log(option.id);
     return option;
 }
 
 // When a spaceship is selected from the list
-shipList.addEventListener("change", selectShipData);
+shipList.addEventListener("change", function(){
+    console.log("Selection made")
+    let id = this.options[this.selectedIndex].id;
+    console.log(this.value);
+    console.log(id);
+    selectShipData(id);
+});
 
 // Retrieve ship data
-function selectShipData() {
-	fetch("https://lldev.thespacedevs.com/2.2.0/config/spacecraft/10")
+function selectShipData(id) {
+	fetch(`https://lldev.thespacedevs.com/2.2.0/config/spacecraft/${id}`)
 		.then(res => res.json())
 		.then(data => {
-			shipImage.innerHTML = `<img src=${data.image_url} alt="ship" />`
+			shipImage.innerHTML = `<img src=${data.image_url} alt="Ship" />`
 		});
 }

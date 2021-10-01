904b2307ec245fdd572dd0e8e271c5079df039c8
Andre <girard176@live.com>
2021-10-01 13:39:06 -0700

    Added shipList and dropdown style

--- main.js
+++ main.js
@@ -9,3 +9,29 @@ const shipDataUrl = "https://lldev.thespacedevs.com/2.2.0/config/spacecraft/?in_
 const shipList = document.getElementById("ship-list");
 const shipImage = document.getElementById("ship-image");
 const shipInfo = document.getElementById("ship-info");
+
+// When the page load
+window.addEventListener("load", updateShipList);
+
+// Retreive the spaceships data
+async function getShipData(){
+    return fetch(shipDataUrl).then(response => response.json());
+}
+
+// Add spaceships to drop down list
+function updateShipList(){
+    getShipData().then(function(data){
+            for(element in data.results){
+                let option = createOption(data.results[element].name);
+                shipList.appendChild(option);
+            }
+        }
+    );
+}
+
+function createOption(text){
+    let option = document.createElement("option");
+    option.textContent = text;
+    console.log(option.text);
+    return option;
+}
